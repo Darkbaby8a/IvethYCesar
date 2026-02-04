147da34a7d3a19f493d4bf046a51d9bfcfb97ac2
Darkbaby8a <44100442+Darkbaby8a@users.noreply.github.com>
2026-02-03 22:29:50 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("September 20, 2025 16:00:00").getTime(),
+let countDown = new Date("May 23, 2026 15:00:00").getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
