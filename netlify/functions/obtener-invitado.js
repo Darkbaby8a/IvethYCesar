4990e9c9f4c6fc18fdaba4918898f6b5b593d426
Darkbaby8a <44100442+Darkbaby8a@users.noreply.github.com>
2026-02-05 16:49:45 -0700

    Update obtener-invitado.js

--- netlify/functions/obtener-invitado.js
+++ netlify/functions/obtener-invitado.js
@@ -23,7 +23,7 @@ export const handler = async (event) => {
     const result = await pool.query(
       `
       SELECT familia, pases, acepto
-      FROM public.invitadosCesar
+      FROM public.invitadoscesar
       WHERE familia = $1
       LIMIT 1;
       `,
